3755ac0fe2f56d1e23a55c5dd92451d048312dc3
RidanDev <gianlucanadir@gmail.com>
2019-03-07 23:57:32 +0100

    Styling using CSS Modules

--- src/pages/about-css-modules.js
+++ src/pages/about-css-modules.js
@@ -2,6 +2,7 @@ import React from "react"
 import Container from "../components/container"
 import Header from "../components/header"
 import { Link } from "gatsby"
+import User from "../components/User"
 
 export default () => (
   <div>
@@ -10,5 +11,15 @@ export default () => (
       <Header headerText="About CSS Modules" />
       <p>CSS Modules are cool</p>
     </Container>
+    <User
+      username="Jane Doe"
+      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+      excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    />
+    <User
+      username="Bob Smith"
+      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
+      excerpt="I'm Bob Smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    />
   </div>
 )
